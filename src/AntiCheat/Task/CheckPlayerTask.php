92bc937c71fa9fca2a7b6780927b793724a9a364
InkoHX <inkohx@gmail.com>
2018-08-28 11:52:53 +0900

    Fix AntiCheat

--- src/AntiCheat/Task/CheckPlayerTask.php
+++ src/AntiCheat/Task/CheckPlayerTask.php
@@ -14,18 +14,19 @@ use AntiCheat\Main;
 class CheckPlayerTask extends PluginTask
 {
     protected $banapi;
+
     public function __construct(Main $plugin)
     {
         parent::__construct($plugin);
         $this->banapi = $this->owner->getServer()->getPluginManager()->getPlugin("BanAPI");
     }
+
     public function onRun(int $currentTick)
     {
         foreach ($this->owner->getServer()->getOnlinePlayers() as $player) {
             if ($player->isOp()) return;
-            if ($player->isFlying()) {
-                $this->banapi->addBan($player->getName(), "Flying(飛行)", "AntiCheat", true);
-            }
+            if (!$player->isFlying()) return;
+            $this->banapi->addBan($player->getName(), "Flying(飛行)", "AntiCheat", true);
         }
     }
 }
