9aceec0439d89a27728232174319307bbaddc06f
iZeaoGamer <jameshayward14@icloud.com>
2019-03-20 12:21:47 +0000

    Update CheckPlayerTask.php

--- src/AntiCheat/Task/CheckPlayerTask.php
+++ src/AntiCheat/Task/CheckPlayerTask.php
@@ -14,18 +14,15 @@ use AntiCheat\Main;
 class CheckPlayerTask extends PluginTask
 {
     protected $banapi;
-
-    public $plugin;
     
     public function __construct(Main $plugin)
-        $this->plugin = $plugin;
     {
         $this->banapi = $this->owner->getServer()->getPluginManager()->getPlugin("BanAPI");
     }
 
     public function onRun(int $currentTick)
     {
-        foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
+        foreach ($this->owner->getServer()->getOnlinePlayers() as $player) {
             if ($player->isOp() || $player->hasPermission("pmessentials.fly") return;
             if (!$player->isFlying()) return;
            $player->close("", "Flight");
